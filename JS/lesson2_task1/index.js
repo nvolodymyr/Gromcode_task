d8e7e525058bd5034a3945f66b6382d06ad5f68a
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-14 15:55:07 +0200

    no message

---
+++ JS/lesson2_task1/index.js
@@ -0,0 +1,8 @@
+const User_Name = 'John';
+const greeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDESSERT = 'cake';
+let pet_ = 'dog';
+let Number;
+Number = 17;
