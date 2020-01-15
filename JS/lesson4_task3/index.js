b89beea07765c3366a54afbe6f7a014ccee0459f
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-15 22:52:03 +0200

    add lesson4_task3

---
+++ JS/lesson4_task3/index.js
@@ -0,0 +1,7 @@
+for (let index = 1; index <= 5; index++) {
+    console.log(`=> ${index} <=`);
+    for (let index1 = 1; index1 <= 10; index1++) {
+        console.log(`${index} x ${index1} = ${index*index1}`);
+    }
+
+}
