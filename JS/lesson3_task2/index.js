6df40119e10186571853bd817a8c472271ad29c4
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-15 16:03:06 +0200

    no message

--- JS/lesson3_task2/index.js
+++ JS/lesson3_task2/index.js
@@ -6,7 +6,7 @@ console.log('null to string is ' + String(null));
 console.log('undefined to string is ' + String(undefined));
 console.log('0 to string is ' + String(0));
 
-console.log("17 to number is " + Number(17));
+console.log("'17' to number is " + Number('17'));
 console.log("true to number is " + Number(true));
 console.log("false to number is " + Number(false));
 console.log("null to number is " + Number(null));
