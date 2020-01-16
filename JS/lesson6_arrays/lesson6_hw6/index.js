d10ffee0d15cb655ac13cd8e4d3a17e42a0862cf
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-17 00:53:14 +0200

    no message

--- JS/lesson6_arrays/lesson6_hw6/index.js
+++ JS/lesson6_arrays/lesson6_hw6/index.js
@@ -3,7 +3,7 @@ function withdraw(clients, balances, client, amount) {
     if (!Array.isArray(balances)) return null;
     for (let index = 0; index < clients.length; index++) {
         if (clients[index] === client) {
-            if (balances[index] - amount > 0) {
+            if (balances[index] - amount >= 0) {
                 balances[index] -= amount;
             } else {
                 return -1;
