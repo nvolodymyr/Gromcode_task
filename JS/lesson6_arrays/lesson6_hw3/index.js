6de9b5c210cd31dbaa0b1acc09b5b9881b4e161a
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-16 23:30:58 +0200

    add lesson6_hw3

---
+++ JS/lesson6_arrays/lesson6_hw3/index.js
@@ -0,0 +1,31 @@
+function sortAsc(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j + 1] < array[j]) {
+                let max = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = max;
+            }
+        }
+    }
+    return array;
+}
+
+function sortDesc(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j + 1] > array[j]) {
+                let max = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = max;
+            }
+        }
+    }
+    return array;
+}
