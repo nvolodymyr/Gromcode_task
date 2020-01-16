a3246cb603fbf3df8c8a75442a58d77c39667732
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-16 22:19:05 +0200

    change from a to index

--- JS/lesson5_functions/lesson5_hw1/index.js
+++ JS/lesson5_functions/lesson5_hw1/index.js
@@ -1,8 +1,8 @@
 function getSum(a, b) {
     let sum = 0;
     for (let index = a; index <= b; index++) {
-        if (a % 2 === 0) {
-            sum += a;
+        if (index % 2 === 0) {
+            sum += index;
         }
 
     }
