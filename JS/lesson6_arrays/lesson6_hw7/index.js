6996c0b787ff17876af3b8b584ef83f697fc67f6
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-17 01:01:56 +0200

    add lesson6_hw7

---
+++ JS/lesson6_arrays/lesson6_hw7/index.js
@@ -0,0 +1,15 @@
+function checker(array) {
+    if (!Array.isArray(array)) return null;
+    let max = array[0];
+    let min = array[0];
+
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] > max) {
+            max = array[i];
+        }
+        if (array[index] < min) {
+            min = array[index];
+        }
+    }
+    return max + min > 1000 ? true : false;
+}
