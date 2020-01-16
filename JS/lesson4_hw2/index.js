0b2388196f521b0c9b663cfffa54e05545b43757
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-16 15:50:18 +0200

    no message

--- JS/lesson4_hw2/index.js
+++ JS/lesson4_hw2/index.js
@@ -4,7 +4,7 @@ let result = 0;
 for (let i = m; i <= n; i++) {
     if ((i % 2 === 0) && (i % 4 !== 0)) {
         result += i;
-    } else if (i % 3 === 3) {
+    } else if (i % 3 === 0) {
         result -= i;
     } else if (i % 4 === 0) {
         result *= i;
