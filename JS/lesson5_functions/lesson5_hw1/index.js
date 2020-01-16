42e50758f8cf804ba486cf37181407e8703befad
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-16 22:17:47 +0200

    no message

--- JS/lesson5_functions/lesson5_hw1/index.js
+++ JS/lesson5_functions/lesson5_hw1/index.js
@@ -1,6 +1,6 @@
 function getSum(a, b) {
     let sum = 0;
-    for (let index = a; index < b; index++) {
+    for (let index = a; index <= b; index++) {
         if (a % 2 === 0) {
             sum += a;
         }
