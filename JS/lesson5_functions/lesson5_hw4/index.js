41ed0e168fe8e7e5d69e6b6513f686704e9dd054
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-16 22:40:31 +0200

    add lesson5_hw4

---
+++ JS/lesson5_functions/lesson5_hw4/index.js
@@ -0,0 +1,12 @@
+let sum = (from, to) => {
+    let sum = 0;
+    for (let index = from; index <= to; index++) {
+        sum += index;
+
+    }
+    return sum;
+}
+
+function compareSums(a, b, c, d) {
+    return sum(a, b) > sum(c, d) ? 'true' : 'false';
+}
