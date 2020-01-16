ba0f8556e217abb5bc037ab86a421ec4a378cbaf
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-16 21:54:17 +0200

    add lesson5_task1

--- JS/lesson5_functions/lesson5_task1/index.js
+++ JS/lesson5_functions/lesson5_task1/index.js
@@ -1,7 +1,3 @@
-for (let index = 1; index <= 5; index++) {
-    console.log(`=> ${index} <=`);
-    for (let index1 = 1; index1 <= 10; index1++) {
-        console.log(`${index} x ${index1} = ${index*index1}`);
-    }
-
+function sum(a, b) {
+    return a + b;
 }
