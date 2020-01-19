bbb5bca0ef540ae0c4c686fb3363d8dfa4261805
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-19 15:54:13 +0200

    add lesson9_hw1

---
+++ JS/lesson9_objects2/lesson9_hw1/index.js
@@ -0,0 +1,15 @@
+let getCustomersList = customObj => {
+    return Object.entries(customObj).map(elem => ({ id: elem[0], ...customObj[elem[0]] }));
+}
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17
+    },
+
+};
+console.log(getCustomersList(customers));
