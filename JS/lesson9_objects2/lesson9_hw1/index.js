05d1fb83869625227b9679cf981fe7b47de38511
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-19 15:56:23 +0200

    add method sort.age

--- JS/lesson9_objects2/lesson9_hw1/index.js
+++ JS/lesson9_objects2/lesson9_hw1/index.js
@@ -1,5 +1,5 @@
 let getCustomersList = customObj => {
-    return Object.entries(customObj).map(elem => ({ id: elem[0], ...customObj[elem[0]] }));
+    return Object.entries(customObj).map(elem => ({ id: elem[0], ...customObj[elem[0]] })).sort((a, b) => a.age - b.age);
 }
 const customers = {
     'customer-id-1': {
