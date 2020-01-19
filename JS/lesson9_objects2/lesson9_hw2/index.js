af34ae86381a51719d3f7a47a4de315908f07a2e
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-19 16:35:06 +0200

    no message

--- JS/lesson9_objects2/lesson9_hw2/index.js
+++ JS/lesson9_objects2/lesson9_hw2/index.js
@@ -1,24 +1,24 @@
-rooms = {
-    room1: [
-        { name: 'room1 name1' },
-        { name: 'room1 name2' },
-        { name: 'room1 name3' },
-        { name: 'room1 name4' },
+// rooms = {
+//     room1: [
+//         { name: 'room1 name1' },
+//         { name: 'room1 name2' },
+//         { name: 'room1 name3' },
+//         { name: 'room1 name4' },
 
-    ],
-    room2: [
-        { name: 'room2 name1' },
+//     ],
+//     room2: [
+//         { name: 'room2 name1' },
 
-    ],
-    room3: [
-        { name: 'room3 name1' },
-        { name: 'room3 name2' },
-        { name: 'room3 name3' },
+//     ],
+//     room3: [
+//         { name: 'room3 name1' },
+//         { name: 'room3 name2' },
+//         { name: 'room3 name3' },
 
-    ],
+//     ],
 
-};
-let getPeople = roomsObj => {
-        return Object.values(roomsObj).reduce((accum, elem) => accum.concat(elem), []).map(elem => elem.name);
-    }
-    // console.log(getPeople(rooms));
+// };
+function getPeople(roomsObj) {
+    return Object.values(roomsObj).reduce((accum, elem) => accum.concat(elem), []).map(elem => elem.name);
+}
+// console.log(getPeople(rooms));
