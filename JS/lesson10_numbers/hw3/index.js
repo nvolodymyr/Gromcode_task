4b207ecb70703624e3e671da7febb8d2a16688aa
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-20 18:06:14 +0200

    no message

---
+++ JS/lesson10_numbers/hw3/index.js
@@ -0,0 +1,9 @@
+let superRoun = (numb, fraction) => {
+    let transformFraction = Math.pow(10, fraction);
+    return [
+        Math.floor(elem * transformFraction) / transformFraction,
+        Math.round(elem * transformFraction) / transformFraction,
+        Math.ceil(elem * transformFraction) / transformFraction,
+        Math.trunc(elem * transformFraction) / transformFraction, +elem.toFixed(fraction)
+    ]
+}
