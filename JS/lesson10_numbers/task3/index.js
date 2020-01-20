322d0cbf055c0f8e7ba87a63d2f6b71842e5d0bf
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-20 14:16:36 +0200

    no message

--- JS/lesson10_numbers/task3/index.js
+++ JS/lesson10_numbers/task3/index.js
@@ -3,6 +3,6 @@ let multiRound = (elem) => {
         Math.floor((elem * 1000)) / 1000,
         Math.round((elem * 1000)) / 1000,
         Math.ceil((elem * 1000)) / 1000,
-        Math.trunc((elem * 1000)) / 1000, +elem.toFixed(2)
+        Math.trunc((elem * 1000)) / 1000, +elem.toFixed(3)
     ]
 }
