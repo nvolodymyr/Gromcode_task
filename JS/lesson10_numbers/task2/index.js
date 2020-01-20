1b4b0db395bceac7ead225d955b482266b5b1d56
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-20 13:36:45 +0200

    add lesson10_task2

---
+++ JS/lesson10_numbers/task2/index.js
@@ -0,0 +1,4 @@
+let getParsedInteger = arr = arr.filter(elem => Number.parseInt(elem));
+let getParsedIntegersV2 = arr = arr.filter(elem => parseInt(elem));
+let getParsedFloats = arr = arr.filter(elem => Number.parseFloat(elem));
+let getParsedFloatsV2 = arr = arr.filter(elem => parseFloat(elem));
