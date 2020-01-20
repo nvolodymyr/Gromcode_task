633af683fcedde634d204d7f5f241e76a7f6020f
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-20 17:53:47 +0200

    no message

--- JS/lesson10_numbers/hw2/index.js
+++ JS/lesson10_numbers/hw2/index.js
@@ -2,8 +2,9 @@ let getRandomNumbers = (lengArray, numberStart, numberFinish) => {
     const notExistDigits = (numberFinish - numberStart < 1) && Math.ceil(numberStart) === Math.ceil(numberFinish);
     if (lengArray <= 0 || (numberFinish < numberStart) || notExistDigits) { return null; }
     let newArray = [];
-    for (let index = 0; index <= lengArray; index++) {
+    for (let index = 0; index < lengArray; index++) {
         newArray[index] = Math.trunc((Math.random() * (numberFinish - numberStart) + numberStart));
     }
     return newArray;
 }
+s
