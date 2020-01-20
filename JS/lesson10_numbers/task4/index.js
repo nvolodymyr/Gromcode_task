8ad50faa5d283e67bf989e635b047ffe2f1fc60c
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-20 15:32:57 +0200

    no message

--- JS/lesson10_numbers/task4/index.js
+++ JS/lesson10_numbers/task4/index.js
@@ -1,5 +1,5 @@
 let getMaxAbsoluteNumber = (arr) => {
-    if (!Array.isArray(arr)) return null;
+    if (!Array.isArray(arr) || arr.length === 0) return null;
     const absoluteValues = arr.map(elem => Math.abs(elem));
     let maxELement = Math.max(...absoluteValues);
     return maxELement;
