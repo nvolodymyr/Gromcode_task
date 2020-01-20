556283d71141b2058ed9566610cf360801b9cebd
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-20 13:21:08 +0200

    no message

--- JS/lesson10_numbers/task1/index.js
+++ JS/lesson10_numbers/task1/index.js
@@ -1,1 +1,5 @@
 let getFiniteNumbers = (arr) => arr.filter(elem => Number.isFinite(elem));
+let getFiniteNumbersV2 = (arr) => arr.filter(elem => isFinite(elem));
+let getNaN = (arr) => arr.filter(elem => Number.isNaN(elem));
+let getNaNV2 = (arr) => arr.filter(elem => isNaN(elem));
+let getIntegers = (arr) => arr.filter(elem => Number.isInteger(elem));
