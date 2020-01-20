2e86bf089641036483803ecb2d9737d4fd8966dc
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-20 15:51:32 +0200

    no message

--- JS/lesson10_numbers/hw1/index.js
+++ JS/lesson10_numbers/hw1/index.js
@@ -1,5 +1,5 @@
 function getTotalPrice(arr) {
     let allDigits = arr.reduce((accum, elem) => accum + elem);
-    let floorDigit = Math.floor(allDigits) * 100 / 100;
+    let floorDigit = (Math.floor(allDigits) * 100) / 100;
     return `$${floorDigit}`;
 }
