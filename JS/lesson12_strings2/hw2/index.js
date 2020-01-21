d116894c8f5a6a5df9f50f4f85397acddfe12052
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-21 23:16:34 +0200

    no message

--- JS/lesson12_strings2/hw2/index.js
+++ JS/lesson12_strings2/hw2/index.js
@@ -1,3 +1,3 @@
 let cleanTransactionsList = arr => {
-    return arr.filter(elem => typeof elem === 'number' && !isNaN(elem)).map(elem => `$${elem.toFixed(2)}`);
+    return arr.filter(elem => typeof + elem === 'number' && !isNaN(elem)).map(elem => `$${(+elem).toFixed(2)}`);
 }
