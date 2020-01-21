f1a90b8b60ca1a3173d4611be76e4492593559fd
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-21 22:55:50 +0200

    no message

--- JS/lesson12_strings2/task1/index.js
+++ JS/lesson12_strings2/task1/index.js
@@ -3,19 +3,19 @@ let calc = str => {
     let result = null;
     switch (a[1]) {
         case '+':
-            result = a[0] + a[2];
-
+            result = +a[0] + a[2];
+            break;
         case '-':
             result = a[0] - a[2];
-
+            break
 
         case '/':
             result = a[0] / a[2];
-
+            break
 
         case '*':
             result = a[0] * a[2];
-
+            break
 
     }
     return `${str}=${result}`;
