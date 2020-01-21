e6d12f363b0388d8d617f3aac41c5bd7d7dcb34d
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-21 22:49:08 +0200

    add lesson12_task1

---
+++ JS/lesson12_strings2/task1/index.js
@@ -0,0 +1,23 @@
+let calc = str => {
+    let a = str.split('');
+    let result = null;
+    switch (a[1]) {
+        case '+':
+            result = a[0] + a[2];
+            break;
+        case '-':
+            result = a[0] - a[2];
+            break;
+
+        case '/':
+            result = a[0] / a[2];
+            break;
+
+        case '*':
+            result = a[0] * a[2];
+            break;
+
+    }
+    return `${str}=${result}`;
+}
+console.log(calc('1+3'));
