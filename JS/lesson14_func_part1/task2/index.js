947242b5dd475b273cfce56c286dd41f4bc691aa
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-22 22:25:53 +0200

    add lesson14_part1_task2

--- JS/lesson14_func_part1/task2/index.js
+++ JS/lesson14_func_part1/task2/index.js
@@ -1,10 +1,21 @@
-let message = 'Just learn it!';
+export default function createMessenger() {
+    let message = 'Just learn it!';
+    let sender = 'Gromcode';
 
-function sendMessage(name) {
-    let sender = 'Gromcode'
-    console.log(`${name}, ${message} Your ${sender}`);
-}
+    function sendMessage(name) {
+        console.log(`${name}, ${message} Your ${sender}`);
+    }
 
-function setMessage(text) {
-    message = text;
+    function setMessage(text) {
+        message = text;
+    }
+
+    function setSender(text) {
+        sender = text;
+    }
+    return {
+        sendMessage,
+        setMessage,
+        setSender
+    }
 }
