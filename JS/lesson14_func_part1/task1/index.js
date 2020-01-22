6088b5d0c165f78343f23dcc9088c1af08132676
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-22 19:04:36 +0200

    no message

--- JS/lesson14_func_part1/task1/index.js
+++ JS/lesson14_func_part1/task1/index.js
@@ -2,6 +2,6 @@ const message = 'Just learn it!';
 let sendMessage = (name) => {
     console.log(`${name}, ${message} Your Gromcode`);
 }
-let setMessag = (str) => {
+export let setMessag = (str) => {
     message = str;
 }
