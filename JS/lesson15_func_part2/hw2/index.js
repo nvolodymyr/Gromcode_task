147a80ae66a232c1361ba849865f2b0aa66b5203
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-23 18:13:51 +0200

    add lesson15_func_part2_hw2

---
+++ JS/lesson15_func_part2/hw2/index.js
@@ -0,0 +1,12 @@
+export let createLogger = () => {
+    let memory = [];
+    return {
+        warn: text => memory.push({ message: text, dateTime: Date.now(), type: 'warn' }),
+        error: text => memory.push({ message: text, dateTime: Date.now(), type: 'error' }),
+        log: text => memory.push({ message: text, dateTime: Date.now(), type: 'log' }),
+        getRecords: textOfType => {
+            let resultArray = textOfType === undefined ? memory : memory.filter(elem => elem.type === textOfType);
+            return resultArray.sort((a, b) => a.dateTime - b.dateTime)
+        }
+    }
+}
