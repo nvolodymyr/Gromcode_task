e08d3efc3a9ba1e993b670e2291067e6c35492d1
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-24 13:02:05 +0200

    no message

--- JS/lesson17_context/hw3/index.js
+++ JS/lesson17_context/hw3/index.js
@@ -1,5 +1,5 @@
 export function bind(func, context, ...args) {
-    return function() {
-        return func.call(context, ...args)
+    return function(...args2) {
+        return func.call(context, ...args, ...args2)
     }
 }
