f1a49d827467b021d7671a588207dad28cd8a78b
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-24 16:03:49 +0200

    no message

--- JS/lesson18_context_part2/hw1/index.js
+++ JS/lesson18_context_part2/hw1/index.js
@@ -1,6 +1,6 @@
 export let spy = (callback) => {
+    wrapFunction.calls = [];
     return function wrapFunction(...argum) {
-        wrapFunction.calls = [];
         wrapFunction.calls.push(argum)
         return callback.apply(this, argum)
     }
