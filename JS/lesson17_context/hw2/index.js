19589ae24c95fdf4887f80a126c98716399c7472
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-24 12:12:19 +0200

    add lesson 17_hw2

---
+++ JS/lesson17_context/hw2/index.js
@@ -0,0 +1,24 @@
+export let timer = {
+    secondsPassed: 0,
+    minsPassed: 0,
+    timerInterval = 0,
+    startTimer() {
+        this.timerInterval = setInterval(() => {
+            this.secondsPassed += 5;
+            if (this.secondsPassed === 60) {
+                this.minsPassed += 1;
+                this.secondsPassed = 0;
+            }
+        }, 5000);
+    },
+    getTime() {
+        return `${this.minsPassed}:${this.secondsPassed<10 ? '0'+this.secondsPassed:this.secondsPassed}}`;
+    },
+    stopTimer() {
+        clearInterval(this.timerInterval);
+    },
+    resetTimer() {
+        this.minsPassed = 0;
+        this.secondsPassed = 0;
+    }
+}
