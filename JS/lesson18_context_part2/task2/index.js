12f7873028ed98fcf5834f02977e6973fc2ac5a0
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-24 14:30:12 +0200

    add lesson18_part2

---
+++ JS/lesson18_context_part2/task2/index.js
@@ -0,0 +1,9 @@
+let wallet = {
+    transactions: [1, 23, 4, 5, 5, 6, 7, 7, 8, 89, 9, -32],
+    getMax() {
+        return Math.max(...this.transactions);
+    },
+    getMin() {
+        return Math.min(...this.transactions);
+    },
+}
