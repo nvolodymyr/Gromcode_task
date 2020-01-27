5a8c658b69d9a714ba1558d125972b2b954bceaf
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-28 00:53:45 +0200

    add lesson 20 task1

---
+++ JS/lesson20_clases/task1/index.js
@@ -0,0 +1,24 @@
+export class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+    setAge(age) {
+        if (age < 0) { return false; }
+        this.age = age;
+        if (age >= 25) {
+            this.requestNewPhoto();
+        }
+        return age;
+    }
+    static createEmpty() {
+        return new User('', null)
+    }
+
+}
