3f3f30f20aeaf95a14aee7614498ff6ea604e0cd
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-27 18:05:13 +0200

    no message

--- JS/lesson19_prototype/task2/index.js
+++ JS/lesson19_prototype/task2/index.js
@@ -1,6 +1,6 @@
 export let getOwnProps = (obj) => {
     let newArray = [];
-    for (const key in object) {
+    for (const key in obj) {
         if (obj.hasOwnProperty(key) && typeof obj[key] !== 'function') {
             newArray.push(key);
 
