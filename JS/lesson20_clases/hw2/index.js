01e5b7cf763fae8e5f27f642088d01f5dd40575f
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-28 14:12:22 +0200

    no message

--- JS/lesson20_clases/hw2/index.js
+++ JS/lesson20_clases/hw2/index.js
@@ -1,23 +1,23 @@
 export class User {
     constructor(id, name, sessionId) {
-        this._id = id;
-        this._name = name;
-        this._sessionId = sessionId;
+        this.id = id;
+        this.name = name;
+        this.sessionId = sessionId;
     }
 
 }
 export class UserRepository {
     constructor(users) {
-        this._users = Object.freeze(users);
+        this.users = Object.freeze(users);
     }
     getUserNames() {
-        return this._users.map(({ _name }) => _name);
+        return this.users.map(({ name }) => name);
     }
     getUserIds() {
-        return this._users.map(({ _id }) => _id);
+        return this.users.map(({ id }) => id);
     }
     getUserNameById(userId) {
-        const user = this._users.find(({ _id }) => _id === userId);
-        return user ? user._name : null;
+        const user = this.users.find(({ id }) => id === userId);
+        return user ? user.name : null;
     }
 }
