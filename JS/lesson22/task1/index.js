bcd0c2d83df4eeaa4a8cebaaca4006dd9e167a54
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-29 18:41:51 +0200

    no message

--- JS/lesson22/task1/index.js
+++ JS/lesson22/task1/index.js
@@ -33,7 +33,7 @@ attachBtn.addEventListener('click', function() {
 });
 attachBtn.click();
 
-
+removehBtn.click();
 removehBtn.addEventListener('click', function() {
     divElem.removeEventListener('click', logGreyDiv, true);
     divElem.removeEventListener('click', logGreenDiv);
@@ -42,7 +42,7 @@ removehBtn.addEventListener('click', function() {
     spanElem.removeEventListener('click', logGreySpan, true);
     spanElem.removeEventListener('click', logGreenSpan);
 });
-// removehBtn.click();
+
 
 const clearAllEvents = () => {
     const eventsListElem = document.querySelector('.events-list');
