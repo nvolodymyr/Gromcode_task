35d30df370fbfd8a20257ba065abaf64b199a2fe
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-29 09:40:52 +0200

    change css file

--- JS/lesson22/task1/index.js
+++ JS/lesson22/task1/index.js
@@ -34,12 +34,12 @@ attachBtn.addEventListener('click', function() {
 
 
 removehBtn.addEventListener('click', function() {
-    divElem.addEventListener('click', logGreyDiv, true);
-    divElem.addEventListener('click', logGreenDiv);
-    pElem.addEventListener('click', logGreyP, true);
-    pElem.addEventListener('click', logGreenP);
-    spanElem.addEventListener('click', logGreySpan, true);
-    spanElem.addEventListener('click', logGreenSpan);
+    divElem.removeEventListener('click', logGreyDiv, true);
+    divElem.removeEventListener('click', logGreenDiv);
+    pElem.removeEventListener('click', logGreyP, true);
+    pElem.removeEventListener('click', logGreenP);
+    spanElem.removeEventListener('click', logGreySpan, true);
+    spanElem.removeEventListener('click', logGreenSpan);
 });
 
 const clearAllEvents = () => {
