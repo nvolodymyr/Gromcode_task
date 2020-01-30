21f0fa75a28da45b5433732b5e3fcb607c55c440
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-01-30 13:37:15 +0200

    add function about trigercheckbox and add new elements

--- JS/lesson22/hw1/index.js
+++ JS/lesson22/hw1/index.js
@@ -1,30 +1,62 @@
 const tasks = [
-    { text: 'Buy milk', done: false, id: 0, date: Date.now(), },
-    { text: 'Pick up Tom from airport', done: false, id: 1, date: Date.now() + 1, },
-    { text: 'Visit party', done: false, id: 2, date: Date.now() + 2, },
-    { text: 'Visit doctor', done: true, id: 3, date: Date.now() + 3, },
-    { text: 'Buy meat', done: true, id: 4, date: Date.now() + 4, },
+    { text: 'Buy milk', done: false, date: Date.now(), },
+    { text: 'Pick up Tom from airport', done: false, date: Date.now() + 1, },
+    { text: 'Visit party', done: false, date: Date.now() + 2, },
+    { text: 'Visit doctor', done: true, date: Date.now() + 3, },
+    { text: 'Buy meat', done: true, date: Date.now() + 4, },
 ]
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
-
-    const listItemsElems = listItems
-        .sort((a, b) => a.done - b.done)
-        .map(({ text, done }) => {
+    listElem.innerHTML = '';
+    const notDoneItemsElements = listItems.filter(elem => elem.done === false)
+        .sort((a, b) => b.date - a.date);
+    const yesDoneItemsElements = listItems.filter(elem => elem.done === true)
+        .sort((a, b) => b.date - a.date);
+    let finalArrayOfItems = [...notDoneItemsElements, ...yesDoneItemsElements]
+    const listItemsElems = finalArrayOfItems
+        .map(({ text, done, date }) => {
             const listItemElem = document.createElement('li');
             listItemElem.classList.add('list__item');
             if (done) {
                 listItemElem.classList.add('list__item_done');
             }
             const cheboxElem = document.createElement('input');
+            cheboxElem.setAttribute('type', 'checkbox');
+            cheboxElem.setAttribute('id', `${date}`)
             cheboxElem.classList.add('list__item-checkbox');
             cheboxElem.checked = done;
-            cheboxElem.setAttribute('type', 'checkbox');
+            cheboxElem.addEventListener('click', trigerCheckbox);
             listItemElem.append(cheboxElem, text);
 
             return listItemElem;
         });
     return listElem.append(...listItemsElems);
+}
+
+////////////////////////////////
+let addNewItem = () => {
+    const nameOfTask = document.querySelector('.task-input');
+    if (nameOfTask.value == '') return;
+    console.log(nameOfTask.value);
+    let newTask = {
+        text: nameOfTask.value,
+        done: false,
+        date: Date.now(),
+    }
+    tasks.push(newTask);
+    renderListItems(tasks);
+    nameOfTask.value = '';
+}
+let createBtn = document.querySelector('.create-task-btn');
+createBtn.addEventListener('click', addNewItem);
+///////////////////////////
+function trigerCheckbox() {
+    let id = this.id;
+    let oneTask = tasks.find(elem => elem.date == id);
+    console.log(oneTask);
+    console.log(this.checked);
+    oneTask.done = this.checked;
+    renderListItems(tasks);
 }
 renderListItems(tasks);
