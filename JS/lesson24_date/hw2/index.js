5bb973145df9417edd934bcb4e0dfb0c69806c6d
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-02-03 01:13:21 +0200

    fix birthDate)))

--- JS/lesson24_date/hw2/index.js
+++ JS/lesson24_date/hw2/index.js
@@ -1,13 +1,13 @@
 export let studentsBirthDays = (students) => {
     let [newArrayOfStudents] = [...students];
     return newArrayOfStudents
-        .sort((a, b) => a.bibirthDate > b.bibirthDate ? 1 : -1)
+        .sort((a, b) => a.birthDate > b.birthDate ? 1 : -1)
         .reduce((acumm, elem) => {
             let monthSortNameOfStudents = [];
-            let nameMonthInDate = new Date(elem.bibirthDate).toString().split(' ')[1];
+            let nameMonthInDate = new Date(elem.birthDate).toString().split(' ')[1];
 
             for (let index = 0; index < newArrayOfStudents.length; index++) {
-                if (nameMonthInDate === new Date(newArrayOfStudents[index].bibirthDate).toString().split(' ')[1]) {
+                if (nameMonthInDate === new Date(newArrayOfStudents[index].birthDate).toString().split(' ')[1]) {
                     monthSortNameOfStudents.push(newArrayOfStudents[index].name);
                 }
 
