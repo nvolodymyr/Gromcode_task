ca469d34396eb6c9b3914abcd0ebace9063c5521
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-02-03 00:33:36 +0200

    add task24_hw1

---
+++ JS/lesson24_date/hw1/index.js
@@ -0,0 +1,25 @@
+const millsecPerMinute = 60 * 1000;
+const millsecPerHour = 60 * millsecPerMinute;
+const millsecPerDay = 24 * millsecPerHour;
+
+
+
+let getDiff = (startDate, endDate) => {
+    ////////////days
+    let differentTime = Math.abs(startDate - endDate);
+    let countDays = Math.trunc(differentTime / millsecPerDay);
+
+    /////hours
+    let countDaysInMilliseconds = countDays * millsecPerDay;
+    let lastDayInMillseconds = differentTime - countDaysInMilliseconds;
+    let countHours = Math.trunc(lastDayInMillseconds / millsecPerHour);
+    /////minutes
+    let lastHourInMs = lastDayInMillseconds - (countHours * millsecPerHour);
+    let minutes = Math.trunc(lastHourInMs / millsecPerMinute);
+    /////secondss
+    let seconds = Math.trunc((lastHourInMs - (minutes * millsecPerMinute)) / 1000)
+
+    return `${countDays}d ${countHours}h ${minutes}m ${seconds}s`
+
+}
+console.log(getDiff(new Date(1991, 2, 22), new Date(1990, 2, 22)));
