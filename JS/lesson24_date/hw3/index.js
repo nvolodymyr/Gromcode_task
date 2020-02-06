484482f56100bcacefb102d3e14a82d49737ea5f
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-02-06 12:31:23 +0200

    fix sorting

--- JS/lesson24_date/hw3/index.js
+++ JS/lesson24_date/hw3/index.js
@@ -1,20 +1,21 @@
 const tasks = [
-    { text: 'Buy milk', done: false, date: Date.now(), },
-    { text: 'Pick up Tom from airport', done: false, date: Date.now() + 1, },
-    { text: 'Visit party', done: false, date: Date.now() + 2, },
-    { text: 'Visit doctor', done: true, date: Date.now() + 3, },
-    { text: 'Buy meat', done: true, date: Date.now() + 4, },
+    { text: 'Buy milk', done: false, date: Date.now(), dateCheck: undefined },
+    { text: 'Pick up Tom from airport', done: false, date: Date.now() + 1, dateCheck: undefined },
+    { text: 'Visit party', done: false, date: Date.now() + 2, dateCheck: undefined },
+    { text: 'Visit doctor', done: true, date: Date.now() + 3, dateCheck: this.date + 1 },
+    { text: 'Buy meat', done: true, date: Date.now() + 4, dateCheck: this.date + 1 },
 ]
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
     listElem.innerHTML = '';
-    const notDoneItemsElements = listItems.filter(elem => elem.done === false)
-        .sort((a, b) => b.date - a.date);
-    const yesDoneItemsElements = listItems.filter(elem => elem.done === true)
-        .sort((a, b) => b.date - a.date);
-    let finalArrayOfItems = [...notDoneItemsElements, ...yesDoneItemsElements]
-    const listItemsElems = finalArrayOfItems
+    // const notDoneItemsElements = listItems.filter(elem => elem.done === false)
+    //     .sort((a, b) => b.date - a.date);
+    // const yesDoneItemsElements = listItems.filter(elem => elem.done === true)
+    //     .sort((a, b) => b.date - a.date);
+    // let finalArrayOfItems = [...notDoneItemsElements, ...yesDoneItemsElements]
+
+    const listItemsElems = listItems.sort((a, b) => b.done - a.done)
         .map(({ text, done, date }) => {
             const listItemElem = document.createElement('li');
             listItemElem.classList.add('list__item');
@@ -38,13 +39,15 @@ const renderListItems = listItems => {
 let addNewItem = () => {
     const nameOfTask = document.querySelector('.task-input');
     if (nameOfTask.value == '') return;
-    console.log(nameOfTask.value);
     let newTask = {
         text: nameOfTask.value,
         done: false,
-        date: Date.now(),
+        date: Date.now() + tasks.length,
+        dateCheck: undefined
     }
-    tasks.push(newTask);
+    let lengthOfDone = tasks.filter(elem => elem.done).length
+
+    tasks.splice(lengthOfDone, 0, newTask);
     renderListItems(tasks);
     nameOfTask.value = '';
 }
@@ -57,7 +60,8 @@ function trigerCheckbox() {
     console.log(oneTask);
     console.log(this.checked);
     oneTask.done = this.checked;
-    oneTask.date = Date.now();
+    oneTask.dateCheck = Date.now();
+
     renderListItems(tasks);
 }
 renderListItems(tasks);
